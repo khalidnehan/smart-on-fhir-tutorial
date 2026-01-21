dea1f16867391e91024ca31c72ae3a57662f544d
khalidnehan <khalidnehan@gmail.com>
2026-01-21 19:03:29 +0400

    Update example-smart-app.js
    Changes if loop for patient name

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -31,9 +31,17 @@
           var fname = '';
           var lname = '';
 
+          //if (typeof patient.name[0] !== 'undefined') {
+            //fname = patient.name[0].given.join(' ');
+            //lname = patient.name[0].family;
+         // }
+
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family;
+            // Handle family as either string or array
+            lname = Array.isArray(patient.name[0].family)
+              ? patient.name[0].family.join(' ')
+              : patient.name[0].family;
           }
 
           var height = byCodes('8302-2');
